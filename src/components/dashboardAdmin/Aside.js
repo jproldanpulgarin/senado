4f6fc198518dc36b2ccddc4ef1011f72d77da645
Juan Pablo Roldan Pulgarin <juan.roldan@est.iudigital.edu.co>
2022-10-18 22:59:50 -0500

    Ya termine por la noche de hoy

--- src/components/dashboardAdmin/Aside.js
+++ src/components/dashboardAdmin/Aside.js
@@ -1,17 +1,22 @@
 import React from "react";
+import Logo_Senado from "../img/Logo_Senado.png"
+import logo_Colombia from "../img/logo_Colombia.png"
+import "../css/Aside.css"
 
 export default function Aside() {
   return (
     <div>
       <aside className="main-sidebar sidebar-dark-primary elevation-4">
         {/* Brand Logo */}
-        <a href="index3.html" className="brand-link">
+        <img src={Logo_Senado} className="logo"/>
+        <a href="index.html" className="brand-link">
           <img
             src="dist/img/AdminLTELogo.png"
             alt="AdminLTE Logo"
             className="brand-image img-circle elevation-3"
             style={{ opacity: ".8" }}
           />
+          
           <span className="brand-text font-weight-light">Admin Dashboard</span>
         </a>
         {/* Sidebar */}
@@ -67,15 +72,7 @@ export default function Aside() {
                   </li>
                 </ul>
               </li>
-              <li className="nav-item">
-                <a href="#" className="nav-link">
-                  <i className="nav-icon fas fa-chart-pie" />
-                  <p>
-                    Charts
-                    <i className="right fas fa-angle-left" />
-                  </p>
-                </a>
-              </li>
+           
               <li className="nav-item">
                 <a href="#" className="nav-link">
                   <i className="nav-icon fas fa-edit" />
@@ -233,7 +230,9 @@ export default function Aside() {
           {/* /.sidebar-menu */}
         </div>
         {/* /.sidebar */}
+        
       </aside>
+      
     </div>
   );
 }
