0a49a33527195119bec68f9906d7422377805e03
Juan Pablo Roldan Pulgarin <juan.roldan@est.iudigital.edu.co>
2022-10-25 15:37:22 -0500

    Realice inserccion de datos mediante un solo componente, no creé mas para agilizar los procesos

--- src/components/dashboardAdmin/crudContent/crudApp.js
+++ src/components/dashboardAdmin/crudContent/crudApp.js
@@ -1,29 +1,144 @@
-import { useState } from 'react';
-import CrudForm from './CrudForm'
-import CrudTable from './CrudTable'
+import React from "react";
+import {
+  Table,
+  Button,
+  Container,
+  Modal,
+  ModalBody,
+  FormGroup,
+  ModalHeader,
+  ModalFooter,
+} from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+const data = [
+  { id: 1, name: "Juan Pablo Roldan Pulgarin", age: 23 },
+  { id: 2, name: "Ana Maria Roldan Pulgarin", age: 21 },
+  { id: 3, name: "Isabella Roldan Pulgarin", age: 15 },
+];
+class CrudApp extends React.Component {
+  state = {
+    data: data,
+    form: {
+      id: "",
+      name: "",
+      age: "",
+    },
+    modalInsertar: false,
+  };
 
 
 
+  handleChange = (e) => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
 
-/* BASE DE DATOS TEMPORAL */
+  mostrarModalInsertar=()=>{
+    this.setState({modalInsertar:true})
+  }
+  ocultarModalInsertar=()=>{
+    this.setState({modalInsertar:false})
+  }
 
-const initialDB=[
-  {id:1,name:"Juan Pablo Roldan Pulgarin",age:23},
-  {id:2,name:"Ana Maria Roldan Pulgarin",age:21},
-  {id:3,name:"Isabella Roldan Pulgarin",age:15}
-]
+  insertar=()=>{
+    var valorNuevo={...this.state.form};
+    valorNuevo.id=this.state.length+1;
+    var lista=this.state.data;
+    lista.push(valorNuevo);
+    this.setState({data:lista,modalInsertar:false})
+  }
 
+  render() {
+    return (
+      <>
+        <Container>
+          <br />
+          <Button color="success" onClick={()=>this.mostrarModalInsertar()}>Agregar Registro</Button>
+          
+          <br />
+          <br />
 
+          <Table>
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Nombre</th>
+                <th>Edad</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.data.map((e) => (
+                <tr>
+                  <td>{e.id}</td>
+                  <td>{e.name}</td>
+                  <td>{e.age}</td>
+                  <td>
+                    <Button color="primary">Editar</Button>{" "}
+                    <Button color="danger">Eliminar</Button>{" "}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Container>
 
-const CrudApp = () => {
-  const [db, setDB] = useState(initialDB);
-  return (
-    <div>
-      <h3>CRUD PARA LA PAGINA</h3>
-      <CrudForm/>
-      <CrudTable data={db}/>
-    </div>
-  )
+        <Modal isOpen={this.state.modalInsertar}>
+          <ModalHeader>
+            <div className="">
+              <h3>Insertar Registros</h3>
+            </div>
+          </ModalHeader>
+          <ModalBody>
+            <FormGroup>
+              <label htmlFor="">Id:</label>
+              <input
+                type="text"
+                name=""
+                id=""
+                readOnly
+                className="form-control"
+                value={this.state.data.length + 1}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label htmlFor="">Nombre:</label>
+              <input
+                type="text"
+                name=""
+                id=""
+                readOnly
+                className="form-control"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label htmlFor="">Edad:</label>
+              <input
+                type="text"
+                name=""
+                id=""
+                readOnly
+                className="form-control"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={()=>this.insertar()}> Insertar</Button>
+            <Button color="danger" onClick={() => this.ocultarModalInsertar()}>Cancelar</Button>
+          </ModalFooter>
+        </Modal>
+      </>
+    );
+  }
 }
-
-export default CrudApp
+export default CrudApp;
