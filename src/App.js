74055e9805bcccbc178d833bcb81cdb634e5a4b6
Juan Pablo Roldan Pulgarin <juan.roldan@est.iudigital.edu.co>
2022-10-24 23:16:29 -0500

    hay q trabajar con modales, dejare la pagina de los reacstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+/* import './App.css';
 
 import Main from './components/pages/main';
 
@@ -10,8 +10,8 @@ function App() {
   );
 }
 
-export default App;
-/* 
+export default App; */
+//https://reactstrap.github.io/?path=/docs/components-button--button
 import React from 'react'
 import Aside from './components/dashboardAdmin/Aside'
 import Content from './components/dashboardAdmin/Content'
@@ -28,4 +28,3 @@ export default function App() {
     </div>
   )
 }
- */
