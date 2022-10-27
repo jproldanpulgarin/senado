b84ab84ab0ae4a7f1d635c91b53b990a0c94a0fd
Juan Pablo Roldan Pulgarin <juan.roldan@est.iudigital.edu.co>
2022-10-27 16:55:09 -0500

    Leyendo node server app fake para validar datos de insercion , get y delete de datos

--- src/components/dashboardAdmin/crudContent/crudApp.js
+++ src/components/dashboardAdmin/crudContent/crudApp.js
@@ -15,24 +15,23 @@ import { faEdit, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Component } from "react";
 
-const url="https://jsonplaceholder.typicode.com/"
+const url = "http://localhost:3001/partidos";
 
 class CrudApp extends Component {
+  //almacenar data
+  state = {
+    data: [],
+  };
 
-//almacenar data
-state={
-  data:[]
-}
-
-peticionGet=()=>{
-  axios.get(url).then(response=>
-    console.log({data: response.data}));
-}
-componentDidMount(){
-this.peticionGet();
-}
-
-
+  peticionGet = () => {
+    axios.get(url).then((response) => {
+      //console.log(response.data); MUESTRA LOS DATOS EN CONSOLA
+      this.setState({ data: response.data });
+    });
+  };
+  componentDidMount() {
+    this.peticionGet();
+  }
 
   render() {
     return (
@@ -53,19 +52,24 @@ this.peticionGet();
             </tr>
           </thead>
           <tbody>
-           {/*  {this.state.data.map(e=>{
-              return(
-                <td>{e.id}</td>
-                <td>{e.nombre}</td>
-                <td>{e.creacion}</td>
-                <td>{e.activo}</td>
-                <td>{e.imagen}</td>
-                <td><button className="btn btn-primary"><FontAwesomeIcon icon={faEdit}/> </button>
-                {""}
-                <td><button className="btn btn-danger"><FontAwesomeIcon icon={faTrashAlt}/> </button>
-                </td>
-              )
-            })} */}
+          
+
+            {this.state.data.map((partidos) => {
+              return (
+                <tr>
+                  <td>{partidos.id}</td>
+                  <td>{partidos.nombre}</td>
+                  <td>{partidos.creacion}</td>
+                  <td>{partidos.activo}</td>
+                  <td>{partidos.imagen}</td>
+                  <td>
+                    <button className="btn btn-primary"><FontAwesomeIcon icon={faEdit}/> </button>
+                    {" "}
+                    <button className="btn btn-danger"><FontAwesomeIcon icon={faTrashAlt}/> </button>
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
